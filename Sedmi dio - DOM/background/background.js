a43382575e351e3164a2625e261e96856bed9729
Kadir Alibasic <kadiralibasic123@gmail.com>
2023-01-24 16:12:55 +0100

    Small change

--- Sedmi dio - DOM/background/background.js
+++ Sedmi dio - DOM/background/background.js
@@ -4,9 +4,10 @@ let h3 = document.querySelector("h3");
 let body = document.getElementById("body");
 let random = document.querySelector("button");
 
-function onLoad() {
-    h3.innerHTML = `linear-gradient(to right, ${input1.value}, ${input2.value})`;
-}
+/* function onLoad() {
+    body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`
+    h3.innerHTML = body.style.background;
+} */
 
 function randomColors() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -15,14 +16,10 @@ function randomColors() {
 function generateColor() {
     input1.value = randomColors();
     input2.value = randomColors();
-    changeColor();
-}
-
-function changeColor() {
-    body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`;
+    body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`
     h3.innerHTML = body.style.background;
 }
 
-input1.addEventListener("input", changeColor);
-input2.addEventListener("input", changeColor);
-random.addEventListener("click", generateColor)
+input1.addEventListener("input", generateColor);
+input2.addEventListener("input", generateColor);
+random.addEventListener("click", generateColor);
